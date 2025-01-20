c5e6ab0617257f6adb3b482e500a85ec281cbdd0
mediateo <mediateo@karts.ac.kr>
2025-01-20 19:41:08 +0900

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  fill(100);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+  text('Spline + p5.js', width/2, height/2);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+// DOM 요소 준비된 후 Spline 씬 로딩 이벤트를 감지
+document.addEventListener('DOMContentLoaded', () => {
+  const loadingScreen = document.getElementById('loadingScreen');
+  const viewer = document.querySelector('spline-viewer');
+  
+  // 씬 로딩 완료시 이벤트 감지
+  viewer.addEventListener('sceneLoad', () => {
+    loadingScreen.style.display = 'none'; // 로딩화면 숨기기
+  });
+});
